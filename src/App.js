e9ed0afcee5f55af8c746c10987b8b8600830d1b
Letícia Cellurale <leticia.cellurale@gmail.com>
2021-01-08 12:02:35 -0300

    :bug: move orders provider to App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import routes from "./config/routing/routes";
 import AppRoute from "./AppRoute";
 import { UserProvider } from "./config/contexts/auth";
 import ProductsProvider from "./config/contexts/products";
+import OrdersProvider from "./config/contexts/orders";
 
 import "./styles/common.css";
 import "./styles/normalize.css";
@@ -12,19 +13,21 @@ const App = () => {
     return (
         <UserProvider>
             <ProductsProvider>
-                <BrowserRouter>
-                    <Switch>
-                        {routes.map((route) => (
-                            <AppRoute
-                                key={route.path}
-                                path={route.path}
-                                exact={true}
-                                component={route.component}
-                                isPrivate={route.isPrivate}
-                            />
-                        ))}
-                    </Switch>
-                </BrowserRouter>
+                <OrdersProvider>
+                    <BrowserRouter>
+                        <Switch>
+                            {routes.map((route) => (
+                                <AppRoute
+                                    key={route.path}
+                                    path={route.path}
+                                    exact={true}
+                                    component={route.component}
+                                    isPrivate={route.isPrivate}
+                                />
+                            ))}
+                        </Switch>
+                    </BrowserRouter>
+                </OrdersProvider>
             </ProductsProvider>
         </UserProvider>
     );
--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -1,16 +1,13 @@
 import CreateOrder from "../../components/sections/CreateOrder";
 import Layout from "../../components/sections/Layout";
 import ListOrders from "../../components/sections/ListOrders";
-import OrdersProvider from "../../config/contexts/orders";
 
 const Order = () => {
     return (
-        <OrdersProvider>
-            <Layout>
-                <CreateOrder />
-                <ListOrders />
-            </Layout>
-        </OrdersProvider>
+        <Layout>
+            <CreateOrder />
+            <ListOrders />
+        </Layout>
     );
 };
 
